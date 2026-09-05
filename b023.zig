9f098292281de3acc31c6ea7dbdf5567d728e892
Vasher <>
2026-09-05 02:47:50 -0700

    add b023 trailer brand solver

---
+++ b023.zig
@@ -0,0 +1,204 @@
+/// Solver for Trailer brand on B223 with Wings and Endless Rod
+const std = @import("std");
+
+pub const Facing = enum(u2) { U, L, R, D };
+pub const Pos = u6; // 36 positions
+
+/// Least significant to most significant bits
+pub const Board = packed struct(u58) {
+    facing: Facing,
+    gray: Pos,
+    pocket: u8, // count of tiles (Endless Rod) - only need u5 for the 18 tiles on B023
+    tiles: u36, // bit set = tile. Cut from u36->u32 due to statues in corners on B223
+    stairs: u6, // 0-35 for a specific tile; 36+ is its position in the pocket
+
+    pub fn at(b: Board, p: Pos) u1 {
+        if (p > 35) unreachable;
+        return @intCast((b.tiles >> p) & 1);
+    }
+
+    fn pickup(b: Board, p: Pos) Board { // t is non-empty
+        if (p > 35) unreachable;
+        const remove_mask: u36 = @as(u36, 1) << p;
+        return Board{
+            .tiles = b.tiles & ~remove_mask,
+            .gray = b.gray,
+            .facing = b.facing,
+            .pocket = b.pocket + 1,
+            .stairs = if (p == b.stairs) 36 + @as(u6, @intCast(b.pocket + 1)) else b.stairs,
+            // .can_z = false,
+        };
+    }
+
+    fn place(b: Board, p: Pos) Board {
+        if (p > 35) unreachable;
+        // guaranteed p is empty
+        if (b.pocket == 0) unreachable;
+        const tile_mask = @as(u36, 1) << p;
+        return Board{
+            .tiles = b.tiles | tile_mask,
+            .gray = b.gray,
+            .facing = b.facing,
+            .pocket = b.pocket - 1,
+            .stairs = if (b.stairs -% 36 == b.pocket) p else b.stairs,
+            // .can_z = false,
+        };
+    }
+    fn move_to(b: Board, p: Pos, f: Facing) ?Board {
+        // Check if the move is allowed
+        if (p == b.stairs) return null;
+        const hovering: bool = b.at(b.gray) == 0;
+        if (hovering and b.at(p) == 0) return null; // cannot continue hovering
+        // update the state as appropriate
+        return Board{
+            .tiles = b.tiles,
+            .gray = p,
+            .facing = f,
+            .pocket = b.pocket,
+            .stairs = b.stairs,
+        };
+    }
+    /// Unmove from the position of `b` to the previous position+facing state (p, f)
+    fn unmove_to(b: Board, p: Pos, f: Facing) Board {
+        return Board{
+            .tiles = b.tiles,
+            .gray = p,
+            .facing = f,
+            .pocket = b.pocket,
+            .stairs = b.stairs,
+        };
+    }
+    pub fn do_action(b: Board, a: Action) ?Board {
+        // prohibit bumping
+        // in this puzzle there's no need to stall; can't move any objects
+        // so it doesnt allow changing facing dir in a useful way
+        switch (a) {
+            .Z => { // symmetrical
+                // get the position in front (pickup/place)
+                const forward = move_by(b.gray, b.facing);
+                if (forward == b.gray) unreachable; // same tile denotes would bump
+                const f_tile = b.at(forward);
+                // pickup if a tile is there
+                // place if its empty
+                return switch (f_tile) {
+                    1 => b.pickup(forward),
+                    0 => b.place(forward),
+                };
+            },
+            else => {
+                const new_facing: Facing = switch (a) {
+                    .U => .U,
+                    .L => .L,
+                    .R => .R,
+                    .D => .D,
+                    else => unreachable,
+                };
+                const new_pos = move_by(b.gray, new_facing);
+                return if (new_pos == b.gray) null else b.move_to(new_pos, new_facing);
+            },
+        }
+    }
+    pub fn reverse(b: Board, a: Action) Board {
+        switch (a) {
+            .Z => { // symmetrical
+                const forward = move_by(b.gray, b.facing);
+                if (forward == b.gray) unreachable; // same tile denotes would bump
+                const f_tile = b.at(forward);
+                // pickup if a tile is there
+                // place if its empty
+                return switch (f_tile) {
+                    1 => b.pickup(forward),
+                    0 => b.place(forward),
+                };
+            },
+            else => {
+                const old_facing: Facing = switch (a) {
+                    .U => .U,
+                    .L => .L,
+                    .R => .R,
+                    .D => .D,
+                    else => unreachable,
+                };
+                const back_dir: Facing = switch (b.facing) {
+                    .U => .D,
+                    .L => .R,
+                    .R => .L,
+                    .D => .U,
+                };
+                const old_pos = move_by(b.gray, back_dir);
+                return b.unmove_to(old_pos, old_facing);
+            },
+        }
+    }
+    /// Z action is available when:
+    ///  - Previous action was not Z  (otherwise we are revisiting a previous state)
+    ///  - Not facing the wall or a rock  (ie, position gray is facing is valid)
+    ///  - We can place from pocket (pocket not empty) or pickup (facing tile not empty)
+    pub fn cant_Z(b: Board, prev: Action) bool {
+        const fw: Pos = move_by(b.gray, b.facing);
+        const tile = b.at(fw);
+        return prev == .Z or (fw == b.gray) or ((b.pocket == 0) and (tile == 0));
+    }
+};
+
+pub const Action = enum(u3) { Z, U, L, R, D };
+
+// B023 has no blockers
+pub fn move_by(p: Pos, f: Facing) Pos {
+    return switch (f) {
+        .U => if (p > 29) p else p + 6,
+        .L => if (p % 6 == 5) p else p + 1,
+        .R => if (p % 6 == 0) p else p - 1,
+        .D => if (p < 6) p else p - 6,
+    };
+}
+
+pub const trailer_tile: u36 = 0b100001_000000_010010_110011_000000_101101;
+
+/// basic heuristic for brand rooms without glass / breakable tiles
+/// Is a consistent heuristic, both with and without the Endless Rod.
+/// However it does not account for hovering state with wings.
+pub fn heuristic(a: u36) u8 {
+    // for each tile that is different, we must either take or place it
+    // Also for each such tile, we must move to face it
+    // (we may already be facing one such tile)
+    return @popCount(a ^ trailer_tile) * 2 - 1;
+}
+
+// B223 start (Stairs appear as a tile here)
+const start_tiles = 0b100101_000110_011111_111110_011000_100001;
+pub const b023 = Board{
+    .tiles = start_tiles,
+    .gray = 15,
+    .facing = .D,
+    .pocket = 0,
+    .stairs = 32,
+};
+
+test "Tile" {
+    try std.testing.expect(b023.at(0) == 1);
+    try std.testing.expect(b023.at(15) == 1);
+    try std.testing.expect(b023.at(32) == 1);
+    try std.testing.expect(b023.at(35) == 1);
+
+    try std.testing.expect(!b023.cant_Z(.D));
+    try std.testing.expect(b023.do_action(.D).?.do_action(.D).?.do_action(.D) == null);
+    try std.testing.expect(b023.do_action(.D).?.do_action(.R).?.do_action(.R) == null);
+    try std.testing.expect(b023.do_action(.U).?.do_action(.D).? == b023);
+    try std.testing.expect(b023.do_action(.D).?.cant_Z(.D));
+    try std.testing.expect(b023.do_action(.U).?.cant_Z(.U));
+}
+
+/// Check whether states are effectively duplicates
+/// Any of:
+///  - They are equal
+///  - They only differ in facing direction and facing direction does not matter
+pub fn is_duplicate_board(a: Board, b: Board, a_cant_z: bool, b_cant_z: bool) bool {
+    return is_duplicate(@bitCast(a), @bitCast(b), a_cant_z, b_cant_z);
+}
+
+pub fn is_duplicate(a: u58, b: u58, a_cant_z: bool, b_cant_z: bool) bool {
+    if (a == b) return true;
+    if (a ^ b > 3) return false;
+    return a_cant_z and b_cant_z;
+}
---
+++ bfs_b023.zig
@@ -0,0 +1,185 @@
+/// BFS searcher for b023
+const std = @import("std");
+
+const brand = @import("b023.zig");
+
+// Limit move depth, if a solution is known to exist within a specific move count
+// ZDZUZRUZUZDRZDUZDLZRZLDZUZLLUDZRZX was previously found manually (and has depth ~34)
+// Running this program finds solutions 1 move shorter (when taking wings into account)
+const MAX_DEPTH: u8 = 34;
+
+const Board = brand.Board;
+const Action = brand.Action;
+const Pos = brand.Pos;
+const b023 = brand.b023;
+const is_duplicate = brand.is_duplicate_board;
+const trailer_tile = brand.trailer_tile;
+
+const Item = packed struct {
+    b: Board,
+    p: Action,
+    cant_z: bool, // precompute
+};
+
+pub fn main(init: std.process.Init) !void {
+    //var gpa = std.heap.DebugAllocator(.{}){};
+    const gpa = init.gpa;
+    //const io = init.io;
+    //const alloc = gpa.allocator();
+    try run_bfs_tile(gpa);
+}
+
+const compressedStream8 = @import("bfs_move.zig").compressedStream8;
+
+/// Backtrace path through state space
+fn trace_path(end: Item, depth: u8, finalized: []const std.ArrayList(Item)) !void {
+    std.debug.print("Found path (reversed): ", .{});
+    defer std.debug.print("\n\n", .{});
+    var cur: Item = end;
+    var b = end.b;
+    var d: u8 = depth;
+    // trace within same tilecount (finalized)
+    while (d > 0) {
+        std.debug.print("{c}", .{@as(u8, switch (cur.p) {
+            .Z => 'Z',
+            else => switch (cur.b.facing) {
+                .U => 'U',
+                .L => 'L',
+                .R => 'R',
+                .D => 'D',
+            },
+        })});
+        b = cur.b.reverse(cur.p);
+        if (b == b023) return;
+        const idx = std.sort.binarySearch(Item, finalized[d - 1].items, b, board_item_cmp).?;
+        cur = finalized[d - 1].items[idx];
+        d -= 1;
+    }
+}
+fn boardCmp(a: Board, b: Board) std.math.Order {
+    const aa: u58 = @bitCast(a);
+    const bb: u58 = @bitCast(b);
+    return if (aa == bb) .eq else if (aa < bb) .lt else .gt;
+}
+
+fn board_item_cmp(a: Board, b: Item) std.math.Order {
+    const aa: u58 = @bitCast(a);
+    const bb: u58 = @bitCast(b.b);
+    return if (aa == bb) .eq else if (aa < bb) .lt else .gt;
+}
+
+const Timestamp = std.Io.Timestamp;
+
+inline fn duplicate_item(a: Item, b: Item) bool {
+    return is_duplicate(a.b, b.b, a.cant_z, b.cant_z);
+}
+fn duplicate_a_subset_of_b(a: Item, b: Item) bool {
+    // if a.cant_z then every move available to A is also available to B, so A is a duplicate
+    // however if A CAN z, and lower-depth visited state B can't, then A allows a new path (assuming facing is diff)
+    return @as(u58, @bitCast(a.b)) ^ @as(u58, @bitCast(b.b)) < 4 and (a.b.facing == b.b.facing or a.cant_z);
+}
+
+// Used only in bucket_contains below, which searches buckets of lower move depth
+fn item_compare(a: Item, b: Item) std.math.Order {
+    if (duplicate_a_subset_of_b(a, b)) return .eq;
+    return if (@as(u58, @bitCast(a.b)) < @as(u58, @bitCast(b.b))) .lt else .gt;
+}
+fn bucket_contains(bucket: std.ArrayList(Item), x: Item) bool {
+    return std.sort.binarySearch(Item, bucket.items, x, item_compare) != null;
+}
+fn item_lessThan(_: void, a: Item, b: Item) bool {
+    return (@as(u58, @bitCast(a.b)) < @as(u58, @bitCast(b.b)));
+}
+
+fn prune(result: Board, depth: u8) bool {
+    // ignore if the goal state is definitely not reachable within MAX_DEPTH total steps
+    return brand.heuristic(result.tiles) + depth > MAX_DEPTH;
+}
+
+const duplicate_stats = true;
+
+fn run_bfs_tile(alloc: std.mem.Allocator) !void {
+    //const t_start = Timestamp.now(io, .awake);
+    // grouped by move depth
+    // individual buckets sorted or otherwise mergeable
+    var todo: std.ArrayList(Item) = .empty;
+    try todo.append(alloc, .{ .b = b023, .p = .D, .cant_z = false });
+    // while iterating through 'todo' we prune as well by checking finalized before inserting
+    // each bucket should be fast to query by Board (at worst O(logn))
+    var finalized = [_]std.ArrayList(Item){.empty} ** MAX_DEPTH;
+    defer {
+        for (&finalized) |*f_bucket| if (f_bucket.items.len > 0) f_bucket.deinit(alloc);
+        if (todo.items.len > 0) todo.deinit(alloc);
+    }
+    //
+    var found = false;
+    for (0..MAX_DEPTH) |depth| {
+        var stats_dupe_depth = if (duplicate_stats) [_]usize{0} ** MAX_DEPTH else void;
+        std.debug.print("sorting and actioning depth {}\n", .{depth});
+        std.sort.pdq(Item, todo.items, {}, item_lessThan);
+        finalized[depth] = try .initCapacity(alloc, todo.items.len);
+        item: for (todo.items, 0..) |b, i| {
+            if (i + 1 < todo.items.len and duplicate_item(b, todo.items[i + 1])) {
+                if (b.cant_z) todo.items[i + 1].cant_z = true;
+                // ^ in this case b.facing==a.facing and using Z would result in an alraedy seen state
+                if (duplicate_stats) stats_dupe_depth[0] += 1;
+                continue;
+            }
+            // Check in previous buckets
+            for (0..depth / 2) |check| {
+                // only check every other move_depth for duplicate states due to parity
+                const check_bucket = finalized[depth - 2 - 2 * check];
+                if (bucket_contains(check_bucket, b)) {
+                    if (duplicate_stats) stats_dupe_depth[check + 1] += 1;
+                    continue :item;
+                }
+            }
+            try finalized[depth].append(alloc, b); // sorted
+        }
+        const dupe_amt = todo.items.len - finalized[depth].items.len;
+        if (duplicate_stats) std.debug.print("deduplicated at depth {}: duplicates {} / {}  ({}%)\n", .{ depth, dupe_amt, todo.items.len, dupe_amt * 100 / todo.items.len });
+        if (duplicate_stats) {
+            std.debug.print("Duplicate depth difference distribution:\n", .{});
+            for (stats_dupe_depth, 0..) |count, i| {
+                if (count > 0) std.debug.print("depth diff -{}: count {}\n", .{ i * 2, count });
+            }
+            std.debug.print("\n", .{});
+        }
+        todo.clearAndFree(alloc);
+        if (depth + 1 == MAX_DEPTH) continue;
+        // generate states for next depth
+        for (finalized[depth].items) |b| {
+            for (std.enums.values(Action)) |a| {
+                if (a == .Z and b.cant_z) continue; // we already computed this so may as well use it
+                if (b.b.do_action(a)) |result| {
+                    if (result.tiles == trailer_tile) {
+                        try trace_path(b, @intCast(depth), finalized[0..depth]);
+                        found = true;
+                        continue;
+                    }
+                    if (prune(result, @as(u8, @intCast(depth + 1)))) continue;
+                    // We don't deduplicate here because it's unordered
+                    try todo.append(alloc, .{ .b = result, .p = switch (a) {
+                        .Z => .Z,
+                        else => switch (b.b.facing) {
+                            .U => .U,
+                            .L => .L,
+                            .R => .R,
+                            .D => .D,
+                        },
+                    }, .cant_z = result.cant_Z(a) });
+                }
+            }
+        }
+        std.debug.print("Finished generating {} states at depth {}\n", .{ todo.items.len, depth + 1 });
+        if (found) break;
+    }
+    std.debug.print("\nDone\n", .{});
+}
+
+fn itemslice_lessThan(_: void, a: []Item, b: []Item) std.math.Order {
+    if (a.len > 0 and b.len > 0) return boardCmp(a[0].b, b[0].b);
+    if (a.len > 0) return .lt;
+    if (b.len > 0) return .gt;
+    return .eq;
+}
